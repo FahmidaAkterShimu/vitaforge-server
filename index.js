1e7f50df31ef38fa3368d7fc7258c65bc2490509
Fahmida Akter Shimu <fahmidaaktershimu@gmail.com>
2026-08-25 22:28:01 +0600

    configure MongoDB connection

--- index.js
+++ index.js
@@ -1,9 +1,39 @@
-require("dotenv").config();
-
 const express = require("express");
+const dotenv = require('dotenv');
 
-const app = express();
-const PORT = process.env.PORT;
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+dotenv.config();
+
+const uri = process.env.MONGODB_URI
+
+const app = express()
+const PORT = process.env.PORT
+
+app.use(express.json());
+
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        await client.connect();
+
+        await client.db("admin").command({ ping: 1 });
+        console.log("You successfully connected to MongoDB!");
+
+
+        return client;
+    } finally {
+        await client.close();
+    }
+}
+run().catch(console.dir);
+
 
 app.get("/", (req, res) => {
     res.json("VitaForge API is running");
